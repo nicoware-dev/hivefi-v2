e8df34ef5b5f26bb5a93b28d2f12225dc6b5671a
0xn1c0 <nicoware.dev@gmail.com>
2025-03-06 13:35:36 -0300

    Update token-transfer.ts

--- eliza/packages/plugin-hivefi/src/multichain/actions/token-transfer.ts
+++ eliza/packages/plugin-hivefi/src/multichain/actions/token-transfer.ts
@@ -20,7 +20,7 @@ const USDC: Token = {
     1: { contractAddress: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48" }, // Ethereum
     10: { contractAddress: "0x7F5c764cBc14f9669B88837ca1490cCa17c31607" }, // Optimism
     42161: { contractAddress: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831" }, // Arbitrum
-    137: { contractAddress: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174" }, // Polygon
+    137: { contractAddress: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359" }, // Polygon
   }
 };
 
